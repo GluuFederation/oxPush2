26693e3b73f7c6a77400a4ec0428145633078d73
NazarYavornytskyy <nazaryavornytskyy@gmail.com>
2016-08-22 17:03:29 +0300

    IOS: more for PODS

---
+++ IOS/Pods/Target Support Files/Pods-oxPush2-IOS/Pods-oxPush2-IOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_oxPush2_IOS : NSObject
+@end
+@implementation PodsDummy_Pods_oxPush2_IOS
+@end
---
+++ IOS/Pods/Target Support Files/Pods-oxPush2-IOS/Pods-oxPush2-IOS-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_oxPush2_IOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_oxPush2_IOSVersionString[];
+
